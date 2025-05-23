08b8a0729b8384487b99855468ab0bc36c652133
jtbermingham <jbermingham@auntap.com>
2025-05-22 22:00:56 -0300

    Juan Tomas Bermingham

--- src/routes/salesRouter.js
+++ src/routes/salesRouter.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { getAllSales } from "../controllers/salesController.js";
+import { getAllSales, getSaleById, getSalesWithTotal, getSalesByCustomerEmail, updateSaleCouponUsed } from "../controllers/salesController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
-router.get("/", getAllSales);
+
+router.get("/", authMiddleware, getAllSales);
+router.get("/total", authMiddleware, getSalesWithTotal);
+router.get("/customer/:email", authMiddleware, getSalesByCustomerEmail);
+router.get("/:id", authMiddleware, getSaleById);
+router.patch("/:id/coupon", authMiddleware, updateSaleCouponUsed);
 
 export default router;
