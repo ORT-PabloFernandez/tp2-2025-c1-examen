cae4695040014b66d78c5bf23c1177428f8e4255
luaramb <arambarrilucia@gmail.com>
2025-05-22 21:24:42 -0300

    Lucia Arambarri

--- src/routes/salesRouter.js
+++ src/routes/salesRouter.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { getAllSales } from "../controllers/salesController.js";
+import { getAllSales, getSale, getSalesTotal, getSalesByUserEmail, updateCouponUsedController, getTopProducts } from "../controllers/salesController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 router.get("/", getAllSales);
+router.get("/total",authMiddleware, getSalesTotal);
+router.get("/top-products",authMiddleware, getTopProducts);
+router.get("/customer/:email",authMiddleware, getSalesByUserEmail);
+router.patch("/:id/coupon",authMiddleware, updateCouponUsedController);
+router.get("/:id",authMiddleware, getSale);
 
 export default router;
--- src/services/salesService.js
+++ src/services/salesService.js
@@ -1,5 +1,74 @@
-import { findAllSales } from "../data/salesData.js";
+import { findAllSales, findAllSalesTotal, findSaleById, findSalesByUserEmail, updateCouponById } from "../data/salesData.js";
 
 export const getSales = async (page, pageSize) => {
     return await findAllSales(page, pageSize);
 }
+
+export const getSaleById = async (id) => {
+    return await findSaleById(id);
+};
+
+export const getSalesWithTotal = async () => {
+    const sales = await findAllSalesTotal();
+
+    const salesWithTotal = sales.map(sale => {
+        const total = sale.items.reduce((acc, item) =>
+            acc + parseFloat(parseFloat(item.price.toString())) * item.quantity,
+        0);
+
+        return { ...sale, 
+            total: `$${total.toFixed(2)}` };
+    });
+
+    return salesWithTotal;
+};
+
+export const getSalesByEmail = async (email) => {
+    const sales = await findSalesByUserEmail(email);
+
+    return sales.map(sale => {
+        const total = sale.items.reduce((acc, item) =>
+            acc + parseFloat(item.price.toString()) * item.quantity,
+        0);
+
+        return {
+            ...sale,
+            total: `$${total.toFixed(2)}`
+        };
+    });
+};
+
+export const updateCouponUsed = async (id, value) => {
+    return await updateCouponById(id, value);
+};
+
+export const getTopSellingProducts = async (limit = 5) => {
+    const sales = await findAllSales();
+    const productMap = {};
+
+    sales.forEach(sale => {
+        sale.items.forEach(item => {
+            const name = item.name;
+            const quantity = item.quantity;
+
+            if (!productMap[name]) {
+                productMap[name] = 0;
+            }
+
+            productMap[name] += quantity;
+        });
+    });
+
+    const productosOrdenados = Object.keys(productMap).map(nombre => {
+        return { product: nombre, totalSold: productMap[nombre] };
+    });
+
+    productosOrdenados.sort((a, b) => b.totalSold - a.totalSold);
+
+    return productosOrdenados.slice(0, limit);
+};
+
+
+
+
+
