7b5ef18bcef9ce93ed53324a6687e7cae619410d
santikhazki <santiagokhazki@gmail.com>
2025-05-22 21:12:37 -0300

    Santiago Khazki

--- src/data/salesData.js
+++ src/data/salesData.js
@@ -1,4 +1,5 @@
 import { getDbSupplies } from "./connection.js";
+import { ObjectId } from "mongodb";
 
 export async function findAllSales(page, pageSize) {
     const db = getDbSupplies();
@@ -16,3 +17,63 @@ export async function findAllSales(page, pageSize) {
         return sales;
     }
 }
+
+export async function findSaleById(id){
+    const db = getDbSupplies();
+    const sale = await db.collection("sales").findOne({_id: new ObjectId(id) });
+    return sale;
+}
+
+export async function findSalesWithTotalAmount(page, pageSize){
+    const sales = await findAllSales(page, pageSize)
+    
+    for (const sale of sales){
+        const items = sale["items"];
+        for(const item of items){
+            item["totalAmount"] = parseFloat(item.price) *  item["quantity"]
+        }
+    }
+
+    return sales;
+
+}
+
+export async function findSaleByCustomerEmail(email){
+    const db = getDbSupplies();
+    const sales = await db.collection("sales").find({ "customer.email": email }).toArray();
+    return sales;
+}
+
+export async function updateCouponValue(id, newValue){
+    const db = getDbSupplies();
+    
+    try {
+    const sales = await db.collection("sales").updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { couponUsed: newValue } }
+    )
+
+    return true;
+    } catch{
+
+    }
+}
+
+export async function findTopProductsBySales(cantidad){
+    const db = getDbSupplies();
+
+    const topProducts = await db.collection("sales").aggregate([
+        { $unwind: "$items" },
+        {
+          $group: {
+            _id: "$items.name",
+            totalSold: { $sum: "$items.quantity" }
+          }
+        },
+        { $sort: { totalSold: -1 } },
+        { $limit: cantidad }
+      ]).toArray();
+
+    return topProducts;
+}
+
--- src/services/salesService.js
+++ src/services/salesService.js
@@ -1,5 +1,25 @@
-import { findAllSales } from "../data/salesData.js";
+import { findAllSales, findSaleById, findSalesWithTotalAmount, findSaleByCustomerEmail, updateCouponValue, findTopProductsBySales } from "../data/salesData.js";
 
 export const getSales = async (page, pageSize) => {
     return await findAllSales(page, pageSize);
+}
+
+export const getSalesById = async (id) => {
+    return await findSaleById(id);
+}
+
+export const getSalesWithTotalAmount = async (page, pageSize) => {
+    return await findSalesWithTotalAmount(page, pageSize);
+}
+
+export const getSalesByCustomerEmail = async (email) => {
+    return await findSaleByCustomerEmail(email);
+}
+
+export const postUpdateCouponValue = async (id, newValue) => {
+    return await updateCouponValue(id, newValue);
+}
+
+export const getTopProductBySales = async (cantidad) => {
+    return await findTopProductsBySales(cantidad);
 }
